624b962baa01ec6a07a820a20cfefce72db909bd
skoh-fley <qhiiyr@gmail.com>
2013-07-01 09:38:23 -0700

    Fixed some input bugs, template gets cloned now

--- webshooter.py
+++ webshooter.py
@@ -2,17 +2,18 @@
 
 import os
 import re
+import subprocess
 import sys
 
-frontname_prompt = """Your website will have two names.
-The frontname is what users will see. It can have spaces.
-A good frontname is 'My First Website'. Enter the frontname now."""
+longname_prompt = """Your website will have two names.
+The longname is what users will see. It can have spaces.
+A good longname is 'My First Website'. Enter the longname now."""
 
-backname_prompt = """The backname is used to name the site on the back end. By convention it should
-be lowercase and use hyphens as spaces. A good backname is 'my-first-website'.
-Enter the backname now."""
+shortname_prompt = """The shortname is used to name the site on the back end. By convention it should
+be lowercase and use hyphens as spaces. A good shortname is 'my-first-website'.
+Enter the shortname now."""
 
-description_prompt = """The description is displayed in your site's header, below the frontname. Enter
+description_prompt = """The description is displayed in your site's header, below the longname. Enter
 the description now."""
 
 templates = {1: ("one.5lab",  "github.com/aims-group/one.5lab"),
@@ -27,7 +28,7 @@ format (#RRGGBB)."""
 
 pages_prompt = """What pages will exist?
   You can change these later. Do not include external links you want in nav.
-  Separate pages with commas. Enter pages in frontname format; backnames will be
+  Separate pages with commas. Enter pages in longname format; shortnames will be
   generated automatically."""
 
 logo_prompt = """Specify a path to a logo which will be copied to your site's directory. If you
@@ -40,20 +41,20 @@ if len(sys.argv) < 2 or sys.argv[1] == "--help":
   )
 else:
   if sys.argv[1] == "new":
-# Frontname
-    print(frontname_prompt)
+# longname
+    print(longname_prompt)
     cin = None
-    frontname = ""
-    while frontname is "":
-      frontname = input("frontname> ").strip()
-# Backname
-    print(backname_prompt)
-    backname = ""
-    while backname is "":
-      backname = input("backname> ").strip()
-      if re.search("\s", backname) is not None:
-        backname = None
-        print("The backname cannot have spaces, try again.")
+    longname = ""
+    while longname is "":
+      longname = input("longname> ").strip()
+# shortname
+    print(shortname_prompt)
+    shortname = ""
+    while shortname is "":
+      shortname = input("shortname> ").strip()
+      if re.search("\s", shortname) is not None:
+        shortname = ""
+        print("The shortname cannot have spaces, try again.")
 # Description
     print(description_prompt)
     description = ""
@@ -81,9 +82,10 @@ else:
       pages = [ p.strip() for p in input("pages> ").split(",") ]
 # Logo
     print(logo_prompt)
-    logo = ""
-    while logo is "" or not os.path.exists(logo):
-      logo = input("logo> ").strip()
+    logo = input("logo> ").strip()
+
+# Set up the site
+    subprocess.call(["git", "clone", "git://" + template[1]])
   elif sys.argv[1] == "update":
     print("Not implemented yet")
 
@@ -91,5 +93,7 @@ def check_deps():
 # python3
 # hyde
 #   if not, pip
+  pass
 
 def set_up_hyde():
+  pass
