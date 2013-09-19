ebd22fbbe1365f2e947122572be4d04468f8b8d6
Matthew Harris <mattbenharris@gmail.com>
2013-09-19 16:29:19 -0700

    MBH: cleaning up file gatherer

--- filegatherer.py
+++ filegatherer.py
@@ -28,17 +28,22 @@ infile_contents = file_to_read.readlines()
 file_to_read.close()
 
 for line in infile_contents:
-    resp = requests.get(line)
+    try:
+        line = line[:-1]
+        resp = requests.get(line)
+        if resp.status_code >= 400:
+            print "Sorry, error occurred getting %s" % line
+            print "Status Code: %s" % resp.status_code
+            continue
+    
+        titleList = line.split("/")
+        title = titleList[len(titleList) - 1] + ".html"
+        text = resp.text 
+        file_to_write = open(os.path.join(files, title), "w")
+        file_to_write.write(text.encode("ascii", "xmlcharrefreplace"))
+        file_to_write.close()
+    except: 
+        print "exception file: %s" % line
+        print "exception status: %s" % resp.status_code
 
-    if resp.status_code >= 400:
-        print "Sorry, error occurred getting %s. Moving on" % line
-        break
-    
-    titleList = line.split("/")
-    title = titleList[len(titleList) - 1] + ".html"
-    text = resp.text 
-    file_to_write = open(os.path.join(files, title), "w")
-    file_to_write.write(text.encode("ascii", "xmlcharrefreplace"))
-    file_to_write.close()
-    
-    #time.sleep(3)
+    time.sleep(1)
