6f128628270ae3cc3b11bc818996567a9cd6df94
Matthew Harris <mattbenharris@gmail.com>
2013-09-23 12:43:07 -0700

    MBH: abstraction fun

--- filegatherer.py
+++ filegatherer.py
@@ -1,52 +1,62 @@
 #!/usr/local/bin/python
 
-import shutil
-import argparse
-import os
 import os.path
 import requests
 import time
-from sys import argv, exit
-from htmlcleaner import filtered_text
 from bs4 import BeautifulSoup
 from random import randint
+def get_filesFromList(urlFile):
+    files = "temp_files"
+    resp = ""
 
-parser = argparse.ArgumentParser(description="Get all the pages from a file of links")
-parser.add_argument("urlFile", help="file containing a list of URLs")
+    if not os.path.exists(files):
+        os.makedirs(files)
 
-args = parser.parse_args()
+    file_to_read = open(urlFile)
+    infile_contents = file_to_read.readlines()
+    file_to_read.close()
 
-if not os.path.exists(args.urlFile):
-    stderr.write("Input file %s not found." % args.urlFile)
-    exit()
+    print("This could take as long as %s x 10 seconds" % len(infile_contents))
 
-files = "temp_files"
-if not os.path.exists(files):
-    os.makedirs(files)
-
-file_to_read = open(args.urlFile)
-infile_contents = file_to_read.readlines()
-file_to_read.close()
-
-for line in infile_contents:
+    for line in infile_contents:
     
-    time.sleep(randint(2,9))
+        time.sleep(randint(2,9))
     
-    try:
-        line = line[:-1]
-        resp = requests.get(line)
-        if resp.status_code >= 400:
-            print "Sorry, error occurred getting %s" % line
-            print "Status Code: %s" % resp.status_code
-            continue
+        try:
+            line = line[:-1]
+            resp = requests.get(line)
+            if resp.status_code >= 400:
+                print "Sorry, error occurred getting %s" % line
+                print "Status Code: %s" % resp.status_code
+                continue
     
-        titleList = line.split("/")
-        title = titleList[len(titleList) - 1] + ".html"
-        text = resp.text 
-        file_to_write = open(os.path.join(files, title), "w")
-        file_to_write.write(text.encode("ascii", "xmlcharrefreplace"))
-        file_to_write.close()
-    except: 
-        print "exception file: %s" % line
-        print "exception status: %s" % resp.status_code
+            titleList = line.split("/")
+            title = titleList[len(titleList) - 1] + ".html"
+            text = resp.text 
+            file_to_write = open(os.path.join(files, title), "w")
+            file_to_write.write(text.encode("ascii", "xmlcharrefreplace"))
+            file_to_write.close()
+        except: 
+            print "exception file: %s" % line
+            print "exception status: %s" % resp.status_code
 
+
+if __name__ == "__main__":
+    import shutil
+    import argparse
+    import os
+    from sys import argv, exit
+
+    parser = argparse.ArgumentParser(description="Get all the pages from a file of links")
+    parser.add_argument("urlFile", help="file containing a list of URLs")
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.urlFile):
+        stderr.write("Input file %s not found." % args.urlFile)
+        exit()
+
+    if get_filesFromList(args.urlFile):
+        print "Error"
+    else:
+        print "Done"
--- urlgatherer.py
+++ urlgatherer.py
@@ -1,47 +1,60 @@
 #!/usr/local/bin/python
 
-import shutil
-import argparse
-import os
-import os.path
 import requests
+import os.path
 from urlparse import urlparse
-from sys import argv, exit
-from htmlcleaner import filtered_text
 from bs4 import BeautifulSoup   
+def get_urlList(url, urlFile, resp):
+    file_to_write = open(urlFile, "w")
+    file_to_write.write(resp.text)
+    file_to_write.close()
 
-parser = argparse.ArgumentParser(description="Get all links on a page and save to a file")
-parser.add_argument("url", help="URL which to pull all the links out of")
-parser.add_argument("urlFile", help="Name of the file to save the list of urls in")
-
-
-args = parser.parse_args()
-
-resp = requests.get(args.url)
-if resp.status_code >= 400:
-	print "Sorry, error occurred."
-	exit()
-
-file_to_write = open(args.urlFile, "w")
-file_to_write.write(resp.text)
-file_to_write.close()
-
-infile = open(args.urlFile).readlines()
-clean_string = ''
-for line in infile:
-    clean_string += line
+    infile = open(urlFile).readlines()
+    clean_string = ''
+    for line in infile:
+        clean_string += line
     
-soup = BeautifulSoup(clean_string)
-cleaner_string = ''
+    soup = BeautifulSoup(clean_string)
+    cleaner_string = ''
 
-fullURL = "http://" + urlparse(args.url).hostname
+    fullURL = "http://" + urlparse(url).hostname
 
-for a in soup.find_all('a', href=True):
+    for a in soup.find_all('a', href=True):
         
-    if a['href'][0] != '#' and a['href'][:4] != "http" and a['href'].find("?") == -1 :
-        nextURL = fullURL + a['href']
-        cleaner_string += "%s\n" % nextURL
-        outfile = open("urlList", "w")
-        outfile.write(cleaner_string)
-        outfile.close()
+        if a['href'][0] != '#' and a['href'][:4] != "http" and a['href'].find("?") == -1 :
+            if a['href'][0] == "/":
+                nextURL = fullURL + a['href']
+            else:
+                nextURL = fullURL + "/" + a['href']
+            
+            cleaner_string += "%s\n" % nextURL
+            outfile = open(urlFile, "w")
+            outfile.write(cleaner_string)
+            outfile.close()
 
+    print ("Created list of URLs in %s" % urlFile)
+    return
+
+if __name__ == "__main__":
+    import shutil
+    import argparse
+    import os
+    from sys import argv, exit
+
+    parser = argparse.ArgumentParser(description="Get all links on a page and save to a file")
+    parser.add_argument("url", help="URL which to pull all the links out of")
+    parser.add_argument("urlFile", help="Name of the file to save the list of urls in")
+
+
+    args = parser.parse_args()
+
+    resp = requests.get(args.url)
+    if resp.status_code >= 400:
+        print "Sorry, error occurred."
+        exit()
+
+
+    if get_urlList(args.url, args.urlFile, resp):
+        print "Error"
+    else:
+        print "Done"
