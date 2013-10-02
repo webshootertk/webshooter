49a49e15f954356801de2fe32d895794b741caa6
Matthew Harris <mattbenharris@gmail.com>
2013-10-02 12:54:05 -0700

    MBH: cleanup

--- sitegatherer.py
+++ sitegatherer.py
@@ -4,18 +4,26 @@ import os.path
 import requests
 import time
 import urlparse
+import sys
+import subprocess
+import select
+import urlparse
 from bs4 import BeautifulSoup
 from random import randint
-import sys
 def get_siteFiles(url, save):
     print "this function is slow on purpose"
-    
+    print " "
+
     if not os.path.exists(save):
         os.makedirs(save)
 
     try:
-       temp = 0
-
+        subprocess.call(["wget", "-r", "--accept=html", "--wait=5", "--random-wait", url])
+        
+        folder = urlparse.urlparse(url)
+        folder = folder.netloc
+        os.rename(folder, save)
+    
     except: 
         print sys.exc_info()[0]
 
@@ -26,8 +34,8 @@ if __name__ == "__main__":
     import os
     from sys import argv, exit
 
-    parser = argparse.ArgumentParser(description="Get all the pages of a website")
-    parser.add_argument("url", help="url of site to get (wget -R )")
+    parser = argparse.ArgumentParser(description="Get all the pages of a website (wget -r)")
+    parser.add_argument("url", help="url of site to get")
     parser.add_argument("save", help="folder name to save raw htlm files in")
 
     args = parser.parse_args()
