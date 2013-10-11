667a989503faebf44b9ad50202fd73ace0c6aa94
Matthew Harris <mattbenharris@gmail.com>
2013-10-11 13:32:27 -0700

    "BOT: AUTO UPDATE NODE STATUS"

--- nodechecker.py
+++ nodechecker.py
@@ -1,24 +1,31 @@
 #!/usr/local/bin/python
 
-import requests
+import subprocess
+import urllib
 import shutil
 import argparse
 import os
 import os.path
+import traceback
 from sys import argv, exit
 
-#path = "/Users/harris112/Projects/ESGF/esgf.github.io.wiki/Shutdown-Status.md"
-path = "urlList"
+path = "/Users/harris112/Projects/ESGF/esgf.github.io.wiki"
+path_to_file = "/Users/harris112/Projects/ESGF/esgf.github.io.wiki/Shutdown-Status.md"
 node = "Node "
 faq = "question "
+node_manager = "esgf-node-manager"
+esgf_web_fe = "esgf-web-fe"
+thredds = "thredds"
+askbot = "Askbot"
 rows = []
 urls = []
 newfile = []
 end_file_flag = False
-##  cd /Users/harris112/Projects/ESGF/esgf.github.io.wiki
-##  git pull
 
-infile = open(path).readlines()
+subprocess.call(["cd", path])
+subprocess.call(["git", "pull"])
+
+infile = open(path_to_file).readlines()
 clean_string = ''
 for line in infile:
     line = line.strip()
@@ -35,29 +42,29 @@ for line in infile:
                 newfile.append(line) 
                 continue
             else:
-                md = rows[1]
-                urls = md.split("][")
-                url = urls[0]
-                url = url[2:]
+                url = rows[1].strip()
                 if not url: rows[3] = " down"
                 else:
                     try:
-                        resp = requests.get(url)
-                        if resp.status_code >= 400: rows[3] = " down"
+                        print url
+                        resp = urllib.urlopen(url)
+                        code = resp.next()
+                        if node_manager not in code and esgf_web_fe not in code and thredds not in code: rows[3] = " down"
+                        elif resp.getcode() != 200: rows[3] = " down"
                         else: rows[3] = " up"
-                    except: rows[3] = " down"
-            
+                    except: rows[3] = " down" #print traceback.format_exc()
+
                 temp =  "|".join(rows)
                 newfile.append(temp)
         else: newfile.append(line)
 
-outfile = open("test", "w")
-#outfile = open(path, "w")
+#outfile = open("test.txt", "w")
+outfile = open(path_to_file, "w")
 outfile.write("\n".join(newfile))
 outfile.close()
 
-## git commit -am "BOT: AUTO UPDATE NODE STATUS"
-## git pull
-## git push
+subprocess.call(["git", "commit", "-am", "\"BOT: AUTO UPDATE NODE STATUS\""])
+subprocess.call(["git", "pull"])
+subprocess.call(["git", "push"])
 
 print "done"
