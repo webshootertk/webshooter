062eca2c65eff5219a2170a9fa83324fb3e5d1b1
Matthew Harris <mattbenharris@gmail.com>
2014-03-24 13:31:33 -0700

    MBH: the lazy worst way to update all the repos

---
+++ getallthefiles.py
@@ -0,0 +1,9 @@
+#!bin/python
+import subprocess
+
+subprocess.call("/Users/harris112/Projects/aims/zpull.sh", shell=True)
+subprocess.call("/Users/harris112/Projects/cf-convention/zpull.sh", shell=True)
+subprocess.call("/Users/harris112/Projects/esgf/zpull.sh", shell=True)
+subprocess.call("/Users/harris112/Projects/mattben/zpull.sh", shell=True)
+subprocess.call("/Users/harris112/Projects/uv-cdat/zpull.sh", shell=True)
+print "Done"
