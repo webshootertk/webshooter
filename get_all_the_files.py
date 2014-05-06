21d8f73fafea33d4852a712729e1e6284ce13659
Matthew Harris <mattbenharris@gmail.com>
2014-05-06 09:05:52 -0700

    MBH: back up all the files

--- get_all_the_files.py
+++ get_all_the_files.py
@@ -5,5 +5,6 @@ subprocess.call("/Users/harris112/Projects/aims/zpull.sh", shell=True)
 subprocess.call("/Users/harris112/Projects/cf-convention/zpull.sh", shell=True)
 subprocess.call("/Users/harris112/Projects/esgf/zpull.sh", shell=True)
 subprocess.call("/Users/harris112/Projects/mattben/zpull.sh", shell=True)
+subprocess.call("/Users/harris112/Projects/pcmdi/zpull.sh", shell=True)
 subprocess.call("/Users/harris112/Projects/uv-cdat/zpull.sh", shell=True)
 print "Done"
