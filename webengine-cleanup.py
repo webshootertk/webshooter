1159a15c22bf2b2088466b8bfc03dd389a6a1d59
Matthew Harris <mattbenharris@gmail.com>
2014-07-16 12:16:29 -0700

    MBH: adding in a clean up file

---
+++ webengine-cleanup.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python
+import os
+import subprocess
+
+os.system("rm *.pyc")
+subprocess.call(["rm", "-rf", "completed_files/"])
+subprocess.call(["rm", "-rf", "doc_files/"])
+subprocess.call(["rm", "-rf", "extracted_files/"])
+subprocess.call(["rm", "-rf", "html_dirty_files/"])
+subprocess.call(["rm", "-rf", "html_files/"])
+subprocess.call(["rm", "-rf", "image_files/"])
+subprocess.call(["rm", "-rf", "pdf_files/"])
+subprocess.call(["rm", "-rf", "whole_site/"])
